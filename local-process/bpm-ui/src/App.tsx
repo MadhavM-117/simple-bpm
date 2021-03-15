d08c34919d6b25d849243722613211f1ac9ba668
Madhav M <madhavmanoj117@gmail.com>
2021-03-15 20:52:04 +0530

    implement simple external task
    - Fix bug with JAXB. Refer [stackoverflow](https://stackoverflow.com/questions/58009038/eclipse-4-12-java-lang-classnotfoundexception-com-sun-xml-internal-bind-v2-co)
    - Figure out how to enable CORS for Camunda with SpringBoot
    - Ensure there are no hyphens in topic name. Use camelCase instead.

--- local-process/bpm-ui/src/App.tsx
+++ local-process/bpm-ui/src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Client, logger } from 'camunda-external-task-client-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  
+}
+
+interface IState {
+  processing: boolean;
+}
+
+class App extends React.Component<IProps, IState> {
+  private client: Client;
+
+  constructor(props:{}) {
+    super(props);
+    this.client = new Client({
+      baseUrl: "http://localhost:8080/engine-rest", 
+      use: logger 
+    });
+ 
+
+    
+    this.state = {
+     processing: false
+    }
+  } 
+
+  componentDidMount() {
+    this.client.subscribe("helloWorld", async function({task, taskService}) {
+        console.log("processing task");
+        setTimeout(async () => {
+          console.log("processing finished")
+  
+          await taskService.complete(task);
+        }, 5000);
+    });
+  }
+
+  private async handleTask({task, taskService}: any) {
+    
+  }
+  
+  public render() {
+    const { processing } = this.state;
+    
+    return (
+      <>
+        <div>Hello World</div>
+        {processing ? <div>Processing is currently running...</div> : <></>}
+      </>
+    );
+  }
 }
 
 export default App;
